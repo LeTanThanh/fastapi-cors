67d2c3abf0884dc03b9fd85ddca4dccb729b9e4c
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-15 16:18:44 +0700

    Use CORSMiddleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -39,4 +40,71 @@ It's also possible to declare the list as "*" (a "wildcard") to say that all are
 But that will only allow certain types of communication, excluding everything that involves credentials: Cookie, Authorization headers like those used with Bearer Tokens, etc.
 
 So, for everything to work correctly, it's better to specify explicitly the allowed origins.
+"""
+
+# Use CORSMiddleware
+"""
+You can configure it in your FastAPI application using the CORSMiddleware.
+
+- Import CORSMiddleware.
+- Create a list of allowed origins (as strings).
+- Add it as a "middleware" to your FastAPI application.
+
+You can also specify if your backdend allow:
+- Credentials (Authorization headers, Cookies, etc).
+- Specific HTTP methods (POST, PUT) or all of them the wildcard "*".
+- Specific HTTP headers or all of them with the wildcard "*".
+"""
+
+origins = [
+  "http://localhost.tiangolo.com",
+  "https://localhost.tiangolo.com",
+  "http://localhost",
+  "http://localhost:8080",
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins = origins,
+  allow_credentials = True,
+  allow_methods = ["*"],
+  allow_headers = ["*"]
+)
+
+@app.get("/hello")
+async def hellow():
+  return {"message": "hello"}
+
+"""
+The default parameters used by the CORSMiddleware implementation are restrictive by default,
+so you'll need to explicitly enable particular origins, methods, or headers, in order for browsers to be permitted to use them in a Cross-Domain context.
+
+The following arguments are supported:
+
+- allow_origins: A list of origins that should be permitted to make cross-origin requests.
+E.g. ["https://example.com", "https://www.example.org"].
+You can use ["*"] to allow any origin.
+
+- allow_origin_regex: A rehex string to match against origins that should be permitted to make cross-origin requests.
+E.g. "https://.*\.example\.org".
+
+- allow_methods: A list of HTTP methods that should be allowed for cross-origin requests.
+Defaults to ["GET"].
+You can use ["*"] to allow all standard methods.
+
+- allow_headers: A list of HTTP request headers that should be supported for cross-origin requests.
+Defaults to [].
+You can use ["*"] to allow all headers.
+The "Accept", "Accept-Language" and "Content-Type" headers are always allowed for simple CORS requests.
+
+- allow_credentials: Indicate that cookies should be supported for cross-origin requests.
+Defaults to False.
+Also, allow_origins cannot be set to ["*"] for credentials ro be allowed, origins muist be specified.
+
+- expose_headers: Indicate any response headers that should be made accessible to the browser.
+Default to [].
+
+- max_age: Sets a maximum time in seconds for browser to cache CORS responses.
+
+The middleware responds to two particular types of HTTP request...
 """
