bed7cb18cbabcd814028e6e1e4e31d5c6f05e632
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-15 15:27:03 +0700

    CORS (Cross-Origin Resource Sharing)

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# CORS (Cross-Origin Resource Sharing)
+"""
+CORS or "Cross-Origin Resource Sharing" refers to the situations when a frontend running in a browser has JavaScript code that communicates with a backend,
+and the backend is in a different "origin" than the frontend
+"""
